82a3aa2803e9a10520ac91608aaecf91504a52e1
Laxman Rai <lex.world@protonmail.com>
2022-01-06 11:45:21 +0545

    Middleware for registration completed

--- middlewares/Authentication/User.js
+++ middlewares/Authentication/User.js
@@ -13,7 +13,22 @@ module.exports = {
      * * 2. Check if everything is valid and !null
      * * 3. Continue
      */
-    next();
+
+    const { email, password } = req.body;
+
+    const regexEmail =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const passwordHasUpper = password.match(/[A-Z]/);
+    const passwordHasLower = password.match(/[a-z]/g);
+    const passwordHasNumber = password.match(/[0-9]/g);
+    const isValidEmail = regexEmail.test(email);
+
+    if (isValidEmail && passwordHasUpper && passwordHasLower && passwordHasNumber) next();
+    else
+      res.status(400).json({
+        message: "Invalid email or password",
+        status: 400,
+      });
   },
 
   /**
